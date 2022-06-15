56df668f39ce063eceba88ac73618e9ab28b9997
GwangA <ordin20118@gmail.com>
2022-06-16 02:47:54 +0900

    Modify url string

--- core/settings.py
+++ core/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/admin/static/'
 
 IMAGE_PATH = "/image"
 DATA_ROOT = "/Users/gwanga/datastore"
