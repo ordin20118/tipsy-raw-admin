56245d190e8add1a10a49dda83dec7c3971aca89
GwangA <ordin20118@gmail.com>
2022-06-19 22:53:09 +0900

    ..

--- core/static/assets/js/tipsy_lib.js
+++ core/static/assets/js/tipsy_lib.js
@@ -1,5 +1,10 @@
 
 
+const CONTENT_TYPE_LIQUOR   = 100;
+const CONTENT_TYPE_COCKTAIL = 200;
+const CONTENT_TYPE_INGD     = 300;
+const CONTENT_TYPE_EQUIP    = 400;
+
 function printSuccessPanel(title, message){
     swal(title, message, {
         icon : "success",
