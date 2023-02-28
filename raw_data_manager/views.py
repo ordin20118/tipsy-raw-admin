1cb21f14a930744121c8879f9280653cfe71f708
ordin20118 <75382892+ordin20118@users.noreply.github.com>
2023-02-28 23:07:40 +0900

    [주류관리] 주류 수정 페이지: ajax API 호출로 주류 데이터 조회

--- raw_data_manager/views.py
+++ raw_data_manager/views.py
@@ -427,21 +427,66 @@ def liquor(request):
     
     if request.method == 'GET':
 
-        page = int(request.GET.get('page', 1))
-        perPage = int(request.GET.get('perPage', 10))
-        totalCount = RawLiquor.objects.all().count()    # TODO: 비효율적인 코드
+        liquor_id = int(request.GET.get('liquorId', 0))
 
-        pageInfo = Paging(page,totalCount,perPage)
-        pages = pageInfo.getPages()
-        firstRow = pageInfo.getFirstRow()
+        logger.info("liquor_id: %d" % liquor_id)
 
-        # #liquorList = RawLiquor.objects.order_by('-liquor_id')[firstRow:firstRow+perPage].values()
-        # liquorList = list(RawLiquor.objects.order_by('-liquor_id')[firstRow:firstRow+perPage].values())
-        # serializer = RawLiquorSerializer(liquorList, many=True) 
+        if liquor_id == 0:
+            return Response("", status=status.HTTP_400_BAD_REQUEST)
+
+        # page = int(request.GET.get('page', 1))
+        # perPage = int(request.GET.get('perPage', 10))
+        # totalCount = RawLiquor.objects.all().count()    # TODO: 비효율적인 코드
+
+        # pageInfo = Paging(page,totalCount,perPage)
+        # pages = pageInfo.getPages()
+        # firstRow = pageInfo.getFirstRow()
+
+        # # #liquorList = RawLiquor.objects.order_by('-liquor_id')[firstRow:firstRow+perPage].values()
+        # # liquorList = list(RawLiquor.objects.order_by('-liquor_id')[firstRow:firstRow+perPage].values())
+        # # serializer = RawLiquorSerializer(liquorList, many=True) 
 
     
-        # TODO: join에 대한 내용을 model에 반영해서 조회하기
-        liquorList = JoinedLiquor.objects.order_by('-liquor_id').raw('''
+        # # TODO: join에 대한 내용을 model에 반영해서 조회하기
+        # liquorList = JoinedLiquor.objects.order_by('-liquor_id').raw('''
+        #     SELECT 
+        #         raw_liquor.*,
+        #         categ1.name as category1_name,
+        #         categ2.name as category2_name,
+        #         categ3.name as category3_name,
+        #         categ4.name as category4_name,
+        #         country.name as country_name,
+        #         reg_user.username as reg_admin_name,
+        #         update_user.username as update_admin_name,
+        #         if(image.path is null, 'default', image.path) as rep_img
+        #     FROM tipsy_raw.raw_liquor
+        #     LEFT OUTER JOIN tipsy_raw.raw_category categ1 ON categ1.id = raw_liquor.category1_id
+        #     LEFT OUTER JOIN tipsy_raw.raw_category categ2 ON categ2.id = raw_liquor.category2_id
+        #     LEFT OUTER JOIN tipsy_raw.raw_category categ3 ON categ3.id = raw_liquor.category3_id
+        #     LEFT OUTER JOIN tipsy_raw.raw_category categ4 ON categ4.id = raw_liquor.category4_id
+        #     LEFT OUTER JOIN tipsy_raw.country ON country.country_id = raw_liquor.country_id
+        #     LEFT OUTER JOIN tipsy_raw.auth_user reg_user ON reg_user.id = raw_liquor.reg_admin
+        #     LEFT OUTER JOIN tipsy_raw.auth_user update_user ON update_user.id = raw_liquor.update_admin
+        #     LEFT OUTER JOIN image ON image.content_id = raw_liquor.liquor_id AND image.content_type = 100 AND image.image_type = 0
+        # ''')
+
+
+        # paginator = Paginator(liquorList, perPage)  # 페이지당 10개씩 보여주기
+        # page_obj = paginator.get_page(page)
+
+        # serializer = JoinedLiquorSerializer(page_obj, many=True) 
+        # #serializer = JoinedLiquorSerializer(liquorList, many=True) 
+     
+        # # TODO: 페이징 정보와 함께 묶어서 Serialize 해주고싶지만 하지못함
+        # #sParam = SearchParam()
+        # #sParam.list = liquorList
+        # #res = cserializers.serialize("json", paramList)
+        # #res = json.dumps(sParam, cls=DjangoJSONEncoder)
+        # #res = json.dumps(sParam.paging.__dict__, default=str)
+        
+        # return Response(serializer.data)
+
+        liquor = JoinedLiquor.objects.raw('''
             SELECT 
                 raw_liquor.*,
                 categ1.name as category1_name,
@@ -452,7 +497,7 @@ def liquor(request):
                 reg_user.username as reg_admin_name,
                 update_user.username as update_admin_name,
                 if(image.path is null, 'default', image.path) as rep_img
-            FROM tipsy_raw.raw_liquor
+            FROM raw_liquor
             LEFT OUTER JOIN tipsy_raw.raw_category categ1 ON categ1.id = raw_liquor.category1_id
             LEFT OUTER JOIN tipsy_raw.raw_category categ2 ON categ2.id = raw_liquor.category2_id
             LEFT OUTER JOIN tipsy_raw.raw_category categ3 ON categ3.id = raw_liquor.category3_id
@@ -461,24 +506,11 @@ def liquor(request):
             LEFT OUTER JOIN tipsy_raw.auth_user reg_user ON reg_user.id = raw_liquor.reg_admin
             LEFT OUTER JOIN tipsy_raw.auth_user update_user ON update_user.id = raw_liquor.update_admin
             LEFT OUTER JOIN image ON image.content_id = raw_liquor.liquor_id AND image.content_type = 100 AND image.image_type = 0
-        ''')
-
-
-        paginator = Paginator(liquorList, perPage)  # 페이지당 10개씩 보여주기
-        page_obj = paginator.get_page(page)
-
-        serializer = JoinedLiquorSerializer(page_obj, many=True) 
-        #serializer = JoinedLiquorSerializer(liquorList, many=True) 
-     
-        # TODO: 페이징 정보와 함께 묶어서 Serialize 해주고싶지만 하지못함
-        #sParam = SearchParam()
-        #sParam.list = liquorList
-        #res = cserializers.serialize("json", paramList)
-        #res = json.dumps(sParam, cls=DjangoJSONEncoder)
-        #res = json.dumps(sParam.paging.__dict__, default=str)
+            WHERE raw_liquor.liquor_id = 1
+        ''')[0]
         
+        serializer = JoinedLiquorSerializer(liquor, many=False)
         return Response(serializer.data)
-        
 
     elif request.method == 'POST':
         respone = 'Success Save Liquor Data'
