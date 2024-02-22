f40721843789e83e8bdb6e736181a7f8762fdc86
ordin20118 <75382892+ordin20118@users.noreply.github.com>
2024-02-22 14:37:55 +0900

    ..

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), '172.30.1.32', '', '192.168.25.11', '172.20.10.3']
-ALLOWED_HOSTS = ['*', '192.168.219.106', 'https://tipsy.co.kr', 'https://www.tipsy.co.kr']
+ALLOWED_HOSTS = ['*', '192.168.219.106', 'tipsy.co.kr', 'www.tipsy.co.kr']
 
 CORS_ALLOWED_ORIGINS = [
     "https://tipsy.co.kr",
